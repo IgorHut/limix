4aee4ebd17b78f8985de903a5c5f6c32946602e8
Danilo Horta <danilo.horta@gmail.com>
2017-05-11 13:18:48 +0100

    Test_qtl_lmm

--- limix/qtl/glmm.py
+++ limix/qtl/glmm.py
@@ -16,37 +16,62 @@ def qtl_test_glmm(snps,
                   NumIntervalsDeltaAlt=100,
                   searchDelta=False,
                   verbose=None):
-    """
-    Wrapper function for univariate single-variant association testing
-    using a generalised linear mixed model.
+    """Univariate single-variant association testing using GLMMs.
 
-    Args:
-        snps (array_like):
-            `N` individuals by `S` SNPs.
-        pheno (tuple, array_like):
-            Either a tuple of two arrays of `N` individuals each (Binomial
-            phenotypes) or an array of `N` individuals (Poisson or Bernoulli
-            phenotypes). It does not support missing values yet.
-        lik ({'bernoulli', 'binomial', 'poisson'}):
-            Sample likelihood describing the residual distribution.
-        K (array_like):
-            `N` by `N` covariance matrix (e.g., kinship coefficients).
-        covs (array_like, optional):
-            `N` individuals by `D` covariates.
-            By default, ``covs`` is a (`N`, `1`) array of ones.
-        test ({'lrt'}, optional):
-            Likelihood ratio test (default).
-        NumIntervalsDeltaAlt (int, optional):
-            number of steps for delta optimization on the alternative model.
-            Requires ``searchDelta=True`` to have an effect.
-        searchDelta (bool, optional):
-            if ``True``, delta optimization on the alternative model is
-            carried out. By default ``searchDelta`` is ``False``.
-        verbose (bool, optional):
-            if ``True``, details such as runtime are displayed.
+    Parameters
+    ----------
+    snps : array_like
+        `N` individuals by `S` SNPs.
+    pheno : tuple, array_like
+        Either a tuple of two arrays of `N` individuals each (Binomial
+        phenotypes) or an array of `N` individuals (Poisson or Bernoulli
+        phenotypes). It does not support missing values yet.
+    lik : {'bernoulli', 'binomial', 'poisson'}
+        Sample likelihood describing the residual distribution.
+    K : array_like
+        `N` by `N` covariance matrix (e.g., kinship coefficients).
+    covs : array_like, optional
+        `N` individuals by `D` covariates.
+        By default, ``covs`` is a (`N`, `1`) array of ones.
+    test : {'lrt'}, optional
+        Likelihood ratio test (default).
+    NumIntervalsDeltaAlt : int, optional
+        number of steps for delta optimization on the alternative model.
+        Requires ``searchDelta=True`` to have an effect.
+    searchDelta : bool, optional
+        if ``True``, delta optimization on the alternative model is
+        carried out. By default ``searchDelta`` is ``False``.
+    verbose : bool, optional
+        if ``True``, details such as runtime are displayed.
 
-    Returns:
-        :class:`limix.qtl.LMM`: LIMIX LMM object
+    Returns
+    -------
+    :class:`limix.qtl.LMM`
+        LIMIX LMM object
+
+    Examples
+    --------
+    .. doctest::
+
+        >>> from numpy import dot, sqrt, exp
+        >>> from numpy.random import RandomState
+        >>> from numpy_sugar.linalg import economic_qs
+        >>>
+        >>> from limix.qtl import qtl_test_glmm
+        >>>
+        >>> random = RandomState(0)
+        >>> nsamples = 50
+        >>>
+        >>> X = random.randn(50, 2)
+        >>> G = random.randn(50, 100)
+        >>> K = dot(G, G.T)
+        >>>
+        >>> z = dot(G, random.randn(100)) / sqrt(100)
+        >>> y = random.poisson(exp(z))
+        >>>
+        >>> lmm = qtl_test_glmm(X, y, 'poisson', K)
+        >>> print(lmm.getPv())
+        [[ 0.9449  0.0061]]
     """
     snps = asarray(snps, float)
 
---
+++ limix/qtl/test/test_qtl_lmm.py
@@ -0,0 +1,31 @@
+from numpy import dot
+from numpy.random import RandomState
+from numpy.testing import assert_allclose
+
+from limix.qtl import LMM
+
+
+def test_qtl_lmm():
+
+    random = RandomState(1)
+
+    N = 100
+    S = 1000
+
+    # generate data
+    snps = (random.rand(N, S) < 0.2).astype(float)
+    pheno = random.randn(N, 1)
+    W = random.randn(N, 10)
+    kinship = dot(W, W.T) / float(10)
+
+    # run single-variant associaiton testing with LMM
+    lmm = LMM(snps, pheno, kinship)
+    pv = lmm.getPv()
+    beta = lmm.getBetaSNP()
+    beta_ste = lmm.getBetaSNPste()
+
+    assert_allclose(
+        pv[:, :5].ravel(),
+        [0.85712431, 0.46681538, 0.58717204, 0.55894821, 0.19178414],
+        rtol=1e-5,
+        atol=1e-5)
